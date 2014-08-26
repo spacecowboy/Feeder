445e0f5f5bb142f67d958fe34b4f16d84971e597
Jonas Kalderstam <jonas@kalderstam.se>
2014-08-27 01:50:30 +0200

    Make text selectable. Links are now clickable
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- app/src/main/java/com/nononsenseapps/feeder/ui/ReaderFragment.java
+++ app/src/main/java/com/nononsenseapps/feeder/ui/ReaderFragment.java
@@ -8,11 +8,17 @@ import android.content.Loader;
 import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Layout;
+import android.text.Selection;
+import android.text.Spannable;
 import android.text.Spanned;
+import android.text.style.ClickableSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ShareActionProvider;
@@ -146,6 +152,55 @@ public class ReaderFragment extends Fragment
             mBodyTextView.setText(
                     android.text.Html.fromHtml(mRssItem.getDescription()));
         }
+
+        mBodyTextView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(final View v, final MotionEvent event) {
+                TextView widget = (TextView) v;
+                Object text = widget.getText();
+                if (text instanceof Spanned) {
+                    Spanned buffer = (Spanned) text;
+
+                    int action = event.getAction();
+
+                    if (action == MotionEvent.ACTION_UP ||
+                        action == MotionEvent.ACTION_DOWN) {
+                        int x = (int) event.getX();
+                        int y = (int) event.getY();
+
+                        x -= widget.getTotalPaddingLeft();
+                        y -= widget.getTotalPaddingTop();
+
+                        x += widget.getScrollX();
+                        y += widget.getScrollY();
+
+                        Layout layout = widget.getLayout();
+                        int line = layout.getLineForVertical(y);
+                        int off = layout.getOffsetForHorizontal(line, x);
+
+                        ClickableSpan[] link =
+                                buffer.getSpans(off, off, ClickableSpan.class);
+
+                        // Cant click to the right of a span, if the line ends with the span!
+                        if (x > layout.getLineRight(line)) {
+                            // Don't call the span
+                        } else if (link.length != 0) {
+                            //if (action == MotionEvent.ACTION_UP) {
+                                link[0].onClick(widget);
+                                return true;
+ //                           }
+//                            else if (action == MotionEvent.ACTION_DOWN) {
+//                                Selection.setSelection(buffer,
+//                                        buffer.getSpanStart(link[0]),
+//                                        buffer.getSpanEnd(link[0]));
+//                            return true;
+//                            }
+                        }
+                    }
+                }
+                return false;
+            }
+        });
 
         // Load images in text
         getLoaderManager().restartLoader(TEXT_LOADER, new Bundle(), this);
