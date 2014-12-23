5787774e3de510c926f56ef867437d0ee269c4d7
Jonas Kalderstam <jonas@kalderstam.se>
2014-12-23 17:12:51 +0100

    remove default object
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- server/flaskapp/feeder/__init__.py
+++ server/flaskapp/feeder/__init__.py
@@ -10,16 +10,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# Set up some default values
-class DefaultConfig(object):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///./feeder.db'
-    FEEDER_ALLOW_GOOGLE = True
-    FEEDER_ALLOW_USERPASS = True
-    FEEDER_ALLOW_ANY_GOOGLE = False
-
 # Load defaults
-app.config.from_object(DefaultConfig)
+#app.config.from_object(DefaultConfig)
 
 # Import REST API
 import feeder.rest
