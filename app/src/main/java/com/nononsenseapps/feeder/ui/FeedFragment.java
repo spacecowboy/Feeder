bbf004cb8f8ca415002ee2a332b43092d5157b86
Jonas Kalderstam <jonas@kalderstam.se>
2015-05-12 16:49:13 +0200

    Shared element transition
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- app/src/main/java/com/nononsenseapps/feeder/ui/FeedFragment.java
+++ app/src/main/java/com/nononsenseapps/feeder/ui/FeedFragment.java
@@ -1002,9 +1002,12 @@ public class FeedFragment extends Fragment
                     i.putExtra(BaseActivity.SHOULD_FINISH_BACK, true);
                     ReaderActivity.setRssExtras(i, rssItem);
 
-                    ActivityOptionsCompat options = ActivityOptionsCompat
-                            .makeScaleUpAnimation(view, 0, 0, view.getWidth(),
-                                    view.getHeight());
+//                    ActivityOptionsCompat options = ActivityOptionsCompat
+//                            .makeScaleUpAnimation(view, 0, 0, view.getWidth(),
+//                                    view.getHeight());
+
+                    view.setTransitionName("shareditem");
+                    ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(getActivity(), view, "shareditem");
 
 //                    ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(getActivity(),
 //                            new Pair<View, String>(titleTextView,
