e2e60bdf75e27b93bcca692600b87eba1c40a284
Jonas Kalderstam <jonas@kalderstam.se>
2015-06-19 11:17:06 +0200

    Update migrate script
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- server/flaskapp/migrate_to_neo.py
+++ server/flaskapp/migrate_to_neo.py
@@ -4,153 +4,89 @@ Usage
 '''
 import sys
 from feeder import read_config
-from py2neo import Graph
+from aggregator.database import get_database
+from aggregator.util import timestamp, escapedict
 from datetime import datetime
 import json
 
 
-def presetup(graph):
-    graph.cypher.execute("CREATE CONSTRAINT ON (feed:Feed) ASSERT feed.link IS UNIQUE")
-    #feed_index
-    #graph.cypher.execute("CREATE CONSTRAINT ON (item:Item) ASSERT item.link IS UNIQUE")
-
-    graph.cypher.execute("CREATE CONSTRAINT ON (user:User) ASSERT user.email IS UNIQUE")
+def presetup(db):
+    db.graph.cypher.execute("CREATE CONSTRAINT ON (feed:Feed) ASSERT feed.link IS UNIQUE")
+    db.graph.cypher.execute("CREATE CONSTRAINT ON (user:User) ASSERT user.email IS UNIQUE")
 
 
-def migrate_feeditem(graph, f, i):
-    cph = """MERGE (i:Item {{ link: {link},
-                              title: {title},
-                              description: {description},
-                              title_stripped: {title_stripped},
-                              snippet: {snippet},
-                              timestamp: {timestamp}
-                              {nullables} }})"""
+def migrate_feeditems(db, feed):
+    # Make feed
+    ts = int(1000 * feed.timestamp.timestamp())
 
-    nullables = ''
+    df = dict(timestamp=ts,
+             link=feed.link,
+             title=feed.title,
+             description=feed.description,
+             published=int(1000 * feed.published.timestamp()) if feed.published else ts,
+             etag=feed.etag,
+             modified=feed.modified)
 
-    if i.published is not None:
-        nullables += ", published: {}".format(json.dumps(int(i.published.timestamp())))
+    items = []
+    for item in feed.items:
+        its = int(1000 * item.timestamp.timestamp())
+        ips = int(1000 * item.published.timestamp()) if item.published else its
+        guid = item.guid if item.guid else item.link
 
-    if i.author is not None:
-        nullables += ", author: {}".format(json.dumps(i.author))
+        di = dict(timestamp=its,
+             guid=guid,
+             link=item.link,
+             title=item.title,
+             description=item.description,
+             title_stripped=item.title_stripped,
+             snippet=item.snippet,
+             published=ips,
+             author=item.author,
+             comments=item.comments,
+             enclosure=item.enclosure,
+             image=item.image)
 
-    if i.comments is not None:
-        nullables += ", comments: {}".format(json.dumps(i.comments))
+        items.append(di)
 
-    if i.enclosure is not None:
-        nullables += ", enclosure: {}".format(json.dumps(i.enclosure))
-
-    if i.image is not None:
-        nullables += ", image: {}".format(json.dumps(i.image))
-
-    # Add for real database
-    #if i.json is not None:
-    #    nullables += ", json: {}".format(json.dumps(i.json))
-
-    code = cph.format(link=json.dumps(i.link),
-                      title=json.dumps(i.title),
-                      description=json.dumps(i.description),
-                      timestamp=json.dumps(int(i.timestamp.timestamp())),
-                      title_stripped=json.dumps(i.title_stripped),
-                      snippet=json.dumps(i.snippet),
-                      nullables=nullables)
-
-    graph.cypher.execute(code)
-
-    # Relation
-    rel = """MATCH (i:Item {{ link: {il} }}), (f:Feed {{link: {fl} }})
-             CREATE (i)-[:IN]->(f)""".format(il=json.dumps(i.link),
-                                             fl=json.dumps(f.link))
-    graph.cypher.execute(rel)
+    db.on_synced(df, ts, items)
 
 
-def migrate_feed(graph, feed):
-    cph = """MERGE (f:Feed {{ link: {link},
-                               title: {title},
-                               description: {description},
-                               timestamp: {timestamp}
-                               {nullables} }})"""
-
-    nullables = ''
-
-    if feed.published is not None:
-        nullables += ', published: {}'.format(json.dumps(int(feed.published.timestamp())))
-
-    if feed.etag is not None:
-        nullables += ', etag: {}'.format(json.dumps(feed.etag))
-
-    if feed.modified is not None:
-        nullables += ', modified: {}'.format(json.dumps(feed.modified))
+def migrate_user(db, u):
+    db.merge_user(u.email, u.passwordhash)
 
 
-    code = cph.format(link=json.dumps(feed.link),
-                      title=json.dumps(feed.title),
-                      description=json.dumps(feed.description),
-                      timestamp=json.dumps(int(feed.timestamp.timestamp())),
-                      nullables=nullables)
-
-    graph.cypher.execute(code)
+def migrate_userfeed(db, u, f):
+    db.subscribe(u.email, f.link, f.title, f.tag)
 
 
-def migrate_user(graph, u):
-    cph = """MERGE (u:User {{ email: {email} {nullable} }})"""
-    nullable = ''
-    if u.passwordhash is not None:
-       nullable = ',passwordhash: {pwhash}'.format(pwhash=json.dumps(u.passwordhash))
-
-    graph.cypher.execute(cph.format(email=json.dumps(u.email), nullable=nullable))
-
-
-def migrate_userfeed(graph, u, f):
-    # Subscription
-    cph = """MATCH (u:User {{ email: {email} }}), (f:Feed {{link: {fl} }})
-             CREATE UNIQUE (u)-[:SUBSCRIBES {{ usertitle: {title}, timestamp: {ts} }}]->(f)"""
-
-    graph.cypher.execute(cph.format(email=json.dumps(u.email),
-                    fl=json.dumps(f.link),
-                    title=json.dumps(f.title),
-                    ts=json.dumps(int(datetime.utcnow().timestamp()))))
-
-
-
-def migrate_userdeletion(graph, u, fl):
-    #Relation
-    rel = """MATCH (u:User {{ email: {email} }}), (f:Feed {{link: {fl} }})
-             CREATE (u)-[:UNSUBSCRIBED {{ timestamp: {ts} }}]->(f)"""
-
-    graph.cypher.execute(rel.format(email=json.dumps(u.email),
-                    fl=json.dumps(fl),
-                    ts=json.dumps(int(datetime.utcnow().timestamp()))))
-
+def migrate_userdeletion(db, u, fl):
+    db.unsubscribe(u.email, fl)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 3 or sys.argv[1] != '-c':
         exit(__doc__)
 
-    graph = Graph('http://neo4j:neo@localhost:7474/db/data/')
-    presetup(graph)
+    db = get_database('http://localhost:7575/db/data/')
+    presetup(db)
 
     configfile = sys.argv[2]
     read_config(configfile)
 
     from feeder.models import FeedItem, Feed, UserFeed, User, UserDeletion
 
-    #for i, feed in enumerate(Feed.query.all()):
-        #print("{} Starting {}".format(i, feed.title))
-        #migrate_feed(graph, feed)
-        #for item in feed.items:
-            #migrate_feeditem(graph, feed, item)
-
-
     for user in User.query.all():
         print("User")
-        migrate_user(graph, user)
+        migrate_user(db, user)
 
         for feed in user.feeds.all():
             print("UserFeed")
-            migrate_userfeed(graph, user, feed)
+            migrate_userfeed(db, user, feed)
+            print("FeedItems")
+            feed = feed.feed
+            migrate_feeditems(db, feed, feed.items)
+
 
         for feed in UserDeletion.query.filter(UserDeletion.user_id==user.id).all():
             print("UserDel")
-            migrate_userdeletion(graph, user, feed.link)
+            migrate_userdeletion(db, user, feed.link)
