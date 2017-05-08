12749a102346b70b4de5b87ca1b7cd37a1b632d6
Jonas Kalderstam <jonas@kalderstam.se>
2017-05-08 19:51:26 +0200

    Don't crash when column doesn't exist

--- app/src/main/kotlin/com/nononsenseapps/feeder/util/CursorExtensions.kt
+++ app/src/main/kotlin/com/nononsenseapps/feeder/util/CursorExtensions.kt
@@ -1,16 +1,25 @@
 package com.nononsenseapps.feeder.util
 
 fun android.database.Cursor.getString(column: String): String? {
-    val index = getColumnIndexOrThrow(column)
-    return if (isNull(index)) null else getString(index)
+    val index = getColumnIndex(column)
+    return when {
+        index < 0 -> null
+        else -> if (isNull(index)) null else getString(index)
+    }
 }
 
 fun android.database.Cursor.getLong(column: String): Long? {
-    val index = getColumnIndexOrThrow(column)
-    return if (isNull(index)) null else getLong(index)
+    val index = getColumnIndex(column)
+    return when {
+        index < 0 -> null
+        else -> if (isNull(index)) null else getLong(index)
+    }
 }
 
 fun android.database.Cursor.getInt(column: String): Int? {
-    val index = getColumnIndexOrThrow(column)
-    return if (isNull(index)) null else getInt(index)
+    val index = getColumnIndex(column)
+    return when {
+        index < 0 -> null
+        else -> if (isNull(index)) null else getInt(index)
+    }
 }
