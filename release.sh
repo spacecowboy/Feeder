ab55e14f5efc97243898872ac7251c2ca8b1ef25
Jonas Kalderstam <jonas@kalderstam.se>
2017-10-29 18:30:45 +0100

    Update changelog and version handling in release script

--- release.sh
+++ release.sh
@@ -21,12 +21,26 @@ next_default="$(cat app/build.gradle | grep "versionName" | sed "s|\s*versionNam
 echo >&2 -n "Next version [${next_default}]: "
 read -r next_in
 
-
 if [ -z "${next_in}" ]; then
   NEXT_VERSION="${next_default}"
 else
   NEXT_VERSION="${next_in}"
 fi
+
+CURRENT_CODE="$(cat app/build.gradle | grep "versionCode" | sed "s|\s*versionCode\s*\([0-9]\+\)|\\1|")"
+echo >&2 "Current code ${CURRENT_CODE}"
+
+let next_code_default=CURRENT_CODE+1
+
+echo >&2 -n "Next code [${next_code_default}]: "
+read -r next_code_in
+
+if [ -z "${next_code_in}" ]; then
+  NEXT_CODE="${next_code_default}"
+else
+  NEXT_CODE="${next_code_in}"
+fi
+
 
 CL="# ${NEXT_VERSION}
 "
@@ -81,6 +95,7 @@ cat >&2 "${tmpfile}"
 read -r -p "Write changelog? [y/N] " response
 if [[ "$response" =~ ^[yY]$ ]]
 then
+  cat "${tmpfile}" >"fastlane/metadata/android/en-US/changelogs/${NEXT_CODE}.txt"
 
   PREV=""
   if [ -f CHANGELOG.md ]; then
@@ -93,6 +108,23 @@ $(cat "${tmpfile}")
 ${PREV}
 EOF
 fi
+
+read -r -p "Update gradle versions? [y/N] " response
+if [[ "$response" =~ ^[yY]$ ]]
+then
+  sed -i "s|\(\s*versionCode\s*\)[0-9]\+|\\1${NEXT_CODE}|" app/build.gradle
+  sed -i "s|\(\s*versionName\s*\).*|\\1\"${NEXT_VERSION}\"|" app/build.gradle
+fi
+
+read -r -p "Commit changes? [y/N] " response
+if [[ "$response" =~ ^[yY]$ ]]
+then
+  git add "fastlane/metadata/android/en-US/changelogs/${NEXT_CODE}.txt"
+  git add app/build.gradle
+  git add CHANGELOG.md
+  git commit -m "Releasing ${NEXT_VERSION}"
+fi
+
 
 read -r -p "Make tag? [y/N] " response
 if [[ "$response" =~ ^[yY]$ ]]
