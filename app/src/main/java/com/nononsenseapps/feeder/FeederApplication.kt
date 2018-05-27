42fab284716a00d0e4fb1ebc3087893138d98a16
Jonas Kalderstam <jonas@kalderstam.se>
2018-05-27 23:36:07 +0200

    Fixed https compatibility on older versions of Android
    Bundling Conscrypt (BoringSSL) with the app to add missing cyphers to
    older versions for Android.
    
    Fixes #113

---
+++ app/src/main/java/com/nononsenseapps/feeder/FeederApplication.kt
@@ -0,0 +1,12 @@
+package com.nononsenseapps.feeder
+
+import android.app.Application
+import org.conscrypt.Conscrypt
+import java.security.Security
+
+class FeederApplication: Application() {
+    init {
+        // Install Conscrypt to handle missing SSL cyphers on older platforms
+        Security.insertProviderAt(Conscrypt.newProvider(), 1)
+    }
+}
