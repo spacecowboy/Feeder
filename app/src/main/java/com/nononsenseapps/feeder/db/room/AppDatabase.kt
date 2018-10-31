6d6870439ce5469b5e1790a88c81b3f461c6b6b1
Jonas Kalderstam <jonas@kalderstam.se>
2018-10-31 12:23:01 +0100

    Reference database migrations in a variable

--- app/src/main/java/com/nononsenseapps/feeder/db/room/AppDatabase.kt
+++ app/src/main/java/com/nononsenseapps/feeder/db/room/AppDatabase.kt
@@ -52,11 +52,13 @@ abstract class AppDatabase : RoomDatabase() {
 
         private fun buildDatabase(context: Context): AppDatabase {
             return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
-                    .addMigrations(MIGRATION_5_7, MIGRATION_6_7, MIGRATION_7_8)
+                    .addMigrations(*allMigrations)
                     .build()
         }
     }
 }
+
+val allMigrations = arrayOf(MIGRATION_5_7, MIGRATION_6_7, MIGRATION_7_8, MIGRATION_8_9)
 
 @Suppress("ClassName")
 /**
