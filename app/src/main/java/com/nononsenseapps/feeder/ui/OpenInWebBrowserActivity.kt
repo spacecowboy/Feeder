12af505e2c88b1aef7110585466cd5ad8451d473
Jonas Kalderstam <jonas@kalderstam.se>
2018-11-29 16:56:52 +0100

    Fixed opening in browser from notification not marking as read or dismissing
    Now item will be marked as read and the notification is dismissed too.
    
    Fixes #155

---
+++ app/src/main/java/com/nononsenseapps/feeder/ui/OpenInWebBrowserActivity.kt
@@ -0,0 +1,50 @@
+package com.nononsenseapps.feeder.ui
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Browser.EXTRA_CREATE_NEW_TAB
+import android.util.Log
+import android.widget.Toast
+import com.nononsenseapps.feeder.R
+import com.nononsenseapps.feeder.coroutines.CoroutineScopedActivity
+import com.nononsenseapps.feeder.db.COL_LINK
+import com.nononsenseapps.feeder.db.room.AppDatabase
+import com.nononsenseapps.feeder.db.room.ID_UNSET
+import com.nononsenseapps.feeder.model.cancelNotification
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+/**
+ * Proxy activity to mark item as read and notified in database as well as cancelling the
+ * notification before opening it in the browser
+ */
+class OpenInWebBrowserActivity : CoroutineScopedActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        intent?.let { intent ->
+            val id: Long = intent.data?.lastPathSegment?.toLong() ?: ID_UNSET
+            val link: String? = intent.getStringExtra(COL_LINK)
+
+            launch(Dispatchers.Default) {
+                AppDatabase.getInstance(this@OpenInWebBrowserActivity).feedItemDao().markAsReadAndNotified(id)
+                cancelNotification(this@OpenInWebBrowserActivity, id)
+            }
+
+            if (link != null) {
+                try {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(link)).also {
+                        intent.putExtra(EXTRA_CREATE_NEW_TAB, true)
+                    })
+                } catch (e: Throwable) {
+                    Toast.makeText(this, R.string.no_activity_for_link, Toast.LENGTH_SHORT).show()
+                    Log.e("FeederOpenInWebBrowser", "Failed to start browser", e)
+                }
+            }
+        }
+
+        // Terminate activity immediately
+        finish()
+    }
+}
