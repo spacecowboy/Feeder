70525eb7098d32223202ff80843b562aa1cf73fc
Jonas Kalderstam <jonas@kalderstam.se>
2019-04-24 20:04:53 +0200

    Fixed crash on tablets and added tablet info to bug report template
    Fixes #191

--- app/src/androidTest/java/com/nononsenseapps/feeder/util/BugReportKTest.kt
+++ app/src/androidTest/java/com/nononsenseapps/feeder/util/BugReportKTest.kt
@@ -1,14 +1,15 @@
 package com.nononsenseapps.feeder.util
 
+import android.content.Context
 import android.content.Intent
-import android.content.Intent.EXTRA_EMAIL
-import android.content.Intent.EXTRA_SUBJECT
-import android.content.Intent.EXTRA_TEXT
+import android.content.Intent.*
 import android.net.Uri
 import android.os.Build
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.nononsenseapps.feeder.BuildConfig
+import com.nononsenseapps.feeder.R
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -23,10 +24,24 @@ class BugReportKTest {
             ${BuildConfig.APPLICATION_ID} (flavor ${BuildConfig.FLAVOR.ifBlank { "None" }})
             version ${BuildConfig.VERSION_NAME} (code ${BuildConfig.VERSION_CODE})
             on Android ${Build.VERSION.RELEASE} (SDK-${Build.VERSION.SDK_INT})
+            on a Tablet? No
 
             Describe your issue and how to reproduce it below:
         """.trimIndent(),
-                emailBody())
+                emailBody(false))
+    }
+
+    @Test
+    fun bodyContainsAndroidInformationAsTablet() {
+        assertEquals("""
+            ${BuildConfig.APPLICATION_ID} (flavor ${BuildConfig.FLAVOR.ifBlank { "None" }})
+            version ${BuildConfig.VERSION_NAME} (code ${BuildConfig.VERSION_CODE})
+            on Android ${Build.VERSION.RELEASE} (SDK-${Build.VERSION.SDK_INT})
+            on a Tablet? Yes
+
+            Describe your issue and how to reproduce it below:
+        """.trimIndent(),
+                emailBody(true))
     }
 
     @Test
@@ -46,12 +61,12 @@ class BugReportKTest {
 
     @Test
     fun intentIsCorrect() {
-        val intent = emailBugReportIntent()
+        val intent = emailBugReportIntent(getApplicationContext())
 
         assertEquals(Intent.ACTION_SENDTO, intent.action)
         assertEquals(Uri.parse("mailto:${emailReportAddress()}"), intent.data)
         assertEquals(emailSubject(), intent.getStringExtra(EXTRA_SUBJECT))
-        assertEquals(emailBody(), intent.getStringExtra(EXTRA_TEXT))
+        assertEquals(emailBody(getApplicationContext<Context>().resources.getBoolean(R.bool.isTablet)), intent.getStringExtra(EXTRA_TEXT))
         assertEquals(emailReportAddress(), intent.getStringExtra(EXTRA_EMAIL))
     }
 }
