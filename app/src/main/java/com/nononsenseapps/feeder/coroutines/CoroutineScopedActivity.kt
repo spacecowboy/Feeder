4e8aee56e47fea68b8c4499e3480eaa3ddb84a47
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2019-09-20 22:31:33 +0200

    UI components implement scope via delegation and MainScope

--- app/src/main/java/com/nononsenseapps/feeder/coroutines/CoroutineScopedActivity.kt
+++ app/src/main/java/com/nononsenseapps/feeder/coroutines/CoroutineScopedActivity.kt
@@ -1,33 +1,21 @@
 package com.nononsenseapps.feeder.coroutines
 
 import android.annotation.SuppressLint
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
 
 /**
  * A fragment which is also Coroutine aware.
  *
- * All coroutines started in this fragment are linked to the Fragment's lifecycle. If the fragment
- * is destroyed, then all coroutines are cancelled. Likewise, if any of the coroutines encounter
- * an exception, that cancels the other coroutines
+ * All coroutines started in this fragment are linked to the Activity's lifecycle. If the activity
+ * is destroyed, then all coroutines are cancelled.
  */
 @SuppressLint("Registered")
-open class CoroutineScopedActivity: AppCompatActivity(), CoroutineScope {
-    lateinit var job: Job
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main + job
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        job = Job()
-        super.onCreate(savedInstanceState)
-    }
-
+open class CoroutineScopedActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     override fun onDestroy() {
-        job.cancel()
+        cancel()
         super.onDestroy()
     }
 }
--- app/src/main/java/com/nononsenseapps/feeder/coroutines/CoroutineScopedFragment.kt
+++ app/src/main/java/com/nononsenseapps/feeder/coroutines/CoroutineScopedFragment.kt
@@ -1,30 +1,18 @@
 package com.nononsenseapps.feeder.coroutines
 
-import android.os.Bundle
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
 
 /**
  * A fragment which is also Coroutine aware.
  *
  * All coroutines started in this fragment are linked to the Fragment's lifecycle. If the fragment
- * is destroyed, then all coroutines are cancelled. Likewise, if any of the coroutines encounter
- * an exception, that cancels the other coroutines
+ * is destroyed, then all coroutines are cancelled.
  */
-open class CoroutineScopedFragment: androidx.fragment.app.Fragment(), CoroutineScope {
-    lateinit var job: Job
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main + job
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        job = Job()
-        super.onCreate(savedInstanceState)
-    }
-
+open class CoroutineScopedFragment : androidx.fragment.app.Fragment(), CoroutineScope by MainScope() {
     override fun onDestroy() {
-        job.cancel()
+        cancel()
         super.onDestroy()
     }
 }
