4cfb4556f1c0cb3b5bb64c98013f0e3cf927e5cf
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2021-03-23 23:36:10 +0100

    ConstraintLayout used to implement expandable

--- app/src/main/java/com/nononsenseapps/feeder/model/FeedListViewModel.kt
+++ app/src/main/java/com/nononsenseapps/feeder/model/FeedListViewModel.kt
@@ -1,7 +1,12 @@
 package com.nononsenseapps.feeder.model
 
 import androidx.collection.ArrayMap
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
 import com.nononsenseapps.feeder.base.KodeinAwareViewModel
@@ -9,7 +14,11 @@ import com.nononsenseapps.feeder.db.room.FeedDao
 import com.nononsenseapps.feeder.db.room.ID_ALL_FEEDS
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.stateIn
 import org.kodein.di.Kodein
 import org.kodein.di.generic.instance
 import kotlin.collections.set
@@ -20,7 +29,10 @@ class FeedListViewModel(kodein: Kodein) : KodeinAwareViewModel(kodein) {
     private val feedsWithUnreadCounts = dao.loadLiveFeedsWithUnreadCounts()
 
     val liveFeedsAndTagsWithUnreadCounts: LiveData<List<FeedUnreadCount>> by lazy {
-        liveData<List<FeedUnreadCount>>(viewModelScope.coroutineContext + Dispatchers.Default, 5000L) {
+        liveData<List<FeedUnreadCount>>(
+            viewModelScope.coroutineContext + Dispatchers.Default,
+            5000L
+        ) {
             feedsWithUnreadCounts.collectLatest { feeds ->
                 val topTag = FeedUnreadCount(id = ID_ALL_FEEDS)
                 val tags: MutableMap<String, FeedUnreadCount> = ArrayMap()
@@ -49,5 +61,22 @@ class FeedListViewModel(kodein: Kodein) : KodeinAwareViewModel(kodein) {
                 emit(data)
             }
         }
+    }
+
+    private val _expandedTags: MutableStateFlow<Set<String>> = MutableStateFlow(emptySet())
+
+    val expandedTags: StateFlow<Set<String>>
+        get() = _expandedTags.asStateFlow()
+
+    fun toggleExpansion(tag: String) {
+        _expandedTags.value = _expandedTags.value.toMutableSet().also { set ->
+            if (!set.add(tag)) {
+                set.remove(tag)
+            }
+        }
+    }
+
+    fun onItemClicked(item: FeedUnreadCount) {
+        // TODO
     }
 }
--- app/src/main/java/com/nononsenseapps/feeder/ui/compose/components/NavDrawer.kt
+++ app/src/main/java/com/nononsenseapps/feeder/ui/compose/components/NavDrawer.kt
@@ -1,114 +1,197 @@
 package com.nononsenseapps.feeder.ui.compose.components
 
-import androidx.compose.foundation.Image
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.nononsenseapps.feeder.R
+import com.nononsenseapps.feeder.model.FeedListViewModel
+import com.nononsenseapps.feeder.model.FeedUnreadCount
 
-@Preview
+const val EXPAND_ANIMATION_DURATION = 300
+const val COLLAPSE_ANIMATION_DURATION = 300
+
 @Composable
-private fun ExpandableItem(
-    text: String = "Some tag",
-    unreadCount: Int = 99
-) {
-    Row(
-        modifier = Modifier
-            .padding(start = 0.dp, end = 4.dp, top = 2.dp, bottom = 2.dp)
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
+fun FeedList(feedListViewModel: FeedListViewModel) {
+    val feedsAndTags by feedListViewModel.liveFeedsAndTagsWithUnreadCounts.observeAsState(initial = emptyList())
+    val expandedTags by feedListViewModel.expandedTags.collectAsState()
+
+    LazyColumn(
+
     ) {
-        // TODO width expandableListPreferredItemPaddingLeft
-        Image(
-            // TODO R.drawable.tinted_expand_more
-            painter = painterResource(id = R.drawable.ic_navigation_expand_more),
-            contentDescription = stringResource(id = R.string.toggle_tag_expansion),
-            modifier = Modifier
-                .clickable(onClick = {})
-        )
-        Row(
-            modifier = Modifier
-                .clickable(onClick = {})
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = text,
-                maxLines = 1,
-                modifier = Modifier
-                    .padding(end = 2.dp)
-            )
-            Text(
-                text = unreadCount.toString(),
-                maxLines = 1,
-                modifier = Modifier
-                    .padding(start = 2.dp)
-            )
+        items(feedsAndTags) { item ->
+            val onItemClick = { feedListViewModel.onItemClicked(item) }
+            when {
+                item.isTag -> ExpandableTag(
+                    item = item,
+                    expanded = item.tag in expandedTags,
+                    onToggleExpand = { feedListViewModel.toggleExpansion(item.tag) },
+                    onItemClick = onItemClick
+                )
+                item.isTop -> TopLevelFeed(item = item, onItemClick = onItemClick)
+                item.tag in expandedTags -> ChildFeed(item = item, onItemClick = onItemClick)
+            }
         }
     }
 }
 
 @Preview
 @Composable
-fun TopLevelItem(
-    text: String = "Some feed",
-    unreadCount: Int = 99
+private fun ExpandableTag(
+    item: FeedUnreadCount = FeedUnreadCount(tag = "News tag", unreadCount = 1),
+    expanded: Boolean = true,
+    onToggleExpand: () -> Unit = {},
+    onItemClick: () -> Unit = {},
+) {
+    val transitionState = remember {
+        MutableTransitionState(expanded).apply {
+            targetState = !expanded
+        }
+    }
+    val transition = updateTransition(transitionState)
+
+    val arrowRotationDegree by transition.animateFloat({
+        tween(durationMillis = EXPAND_ANIMATION_DURATION)
+    }) {
+        if (expanded) 0f else 180f
+    }
+    ConstraintLayout(
+        modifier = Modifier
+            .clickable(onClick = onItemClick)
+            .padding(start = 0.dp, end = 4.dp, top = 2.dp, bottom = 2.dp)
+            .fillMaxWidth()
+            .height(48.dp)
+    ) {
+        val (expandButton, text, unreadCount) = createRefs()
+        Text(
+            text = item.unreadCount.toString(),
+            maxLines = 1,
+            modifier = Modifier
+                .padding(start = 2.dp)
+                .constrainAs(unreadCount) {
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                    centerVerticallyTo(parent)
+                }
+        )
+        ExpandArrow(
+            degrees = arrowRotationDegree,
+            onClick = onToggleExpand,
+            modifier = Modifier
+                .constrainAs(expandButton) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                }
+        )
+        Text(
+            text = item.tag,
+            maxLines = 1,
+            modifier = Modifier
+                .padding(end = 2.dp)
+                .constrainAs(text) {
+                    top.linkTo(parent.top)
+                    start.linkTo(expandButton.end)
+                    end.linkTo(unreadCount.start)
+                    width = Dimension.fillToConstraints
+                    centerVerticallyTo(parent)
+                }
+        )
+    }
+}
+
+@Composable
+fun ExpandArrow(
+    degrees: Float,
+    modifier: Modifier,
+    onClick: () -> Unit
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_expand_less_24),
+            contentDescription = stringResource(id = R.string.toggle_tag_expansion),
+            modifier = Modifier.rotate(degrees = degrees)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TopLevelFeed(
+    item: FeedUnreadCount = FeedUnreadCount(title = "A feed", unreadCount = 999),
+    onItemClick: () -> Unit = {}
 ) = Feed(
-    text = text,
-    unreadCount = unreadCount,
-    startPadding = 16.dp
+    text = item.displayTitle,
+    unreadCount = item.unreadCount,
+    startPadding = 16.dp,
+    onItemClick = onItemClick
 )
 
 @Preview
 @Composable
-fun ChildItem(
-    text: String = "Some feed",
-    unreadCount: Int = 99
+fun ChildFeed(
+    item: FeedUnreadCount = FeedUnreadCount(title = "Some feed", unreadCount = 21),
+    onItemClick: () -> Unit = {}
 ) = Feed(
-    text = text,
-    unreadCount = unreadCount,
-    startPadding = 39.dp // TODO expandableListPreferredChildPaddingLeft
+    text = item.displayTitle,
+    unreadCount = item.unreadCount,
+    startPadding = 48.dp,
+    onItemClick = onItemClick
 )
 
 @Composable
 private fun Feed(
     text: String,
     unreadCount: Int,
-    startPadding: Dp
+    startPadding: Dp,
+    onItemClick: () -> Unit
+) = Row(
+    modifier = Modifier
+        .clickable(onClick = onItemClick)
+        .padding(start = startPadding, end = 4.dp, top = 2.dp, bottom = 2.dp)
+        .fillMaxWidth()
+        .height(48.dp),
+    verticalAlignment = Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.SpaceBetween
 ) {
-    Row(
+    Text(
+        text = text,
+        maxLines = 1,
         modifier = Modifier
-            .padding(start = startPadding, end = 4.dp, top = 2.dp, bottom = 2.dp)
-            .clickable(onClick = {})
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(
-            text = text,
-            maxLines = 1,
-            modifier = Modifier
-                .padding(end = 2.dp)
-        )
-        Text(
-            text = unreadCount.toString(),
-            maxLines = 1,
-            modifier = Modifier
-                .padding(start = 2.dp)
-        )
-    }
+            .padding(end = 2.dp)
+    )
+    Text(
+        text = unreadCount.toString(),
+        maxLines = 1,
+        modifier = Modifier
+            .padding(start = 2.dp)
+    )
 }
