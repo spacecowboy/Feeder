afdca6519139389f8059d648b3e74983b73e78a6
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2021-09-20 21:02:30 +0200

    Fixed image placeholders

--- app/src/main/java/com/nononsenseapps/feeder/ui/compose/feed/FeedScreen.kt
+++ app/src/main/java/com/nononsenseapps/feeder/ui/compose/feed/FeedScreen.kt
@@ -283,7 +283,8 @@ fun FeedScreen(
                                 painter = rememberImagePainter(
                                     data = imageUrl,
                                     builder = {
-                                        this.error(placeHolder)
+                                        this.placeholder(placeHolder)
+                                            .error(placeHolder)
                                     },
                                 ),
                                 contentScale = ContentScale.Crop,
