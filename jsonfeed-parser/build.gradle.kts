a442e159ef31425e62fc032d6f87a81c3b906672
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2022-08-17 20:55:43 +0200

    Upgraded Android Gradle Plugin and such

--- jsonfeed-parser/build.gradle.kts
+++ jsonfeed-parser/build.gradle.kts
@@ -13,32 +13,32 @@ configurations.all {
     resolutionStrategy {
         failOnVersionConflict()
 
-        val kotlin_version: String by project
+        val kotlinVersion: String by project
         val okio_version: String by project
 
         force("com.squareup.okio:okio:$okio_version")
-        force("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
-        force("org.jetbrains.kotlin:kotlin-stdlib-common:$kotlin_version")
-        force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
-        force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version")
-        force("org.jetbrains.kotlin:kotlin-reflect:$kotlin_version")
+        force("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+        force("org.jetbrains.kotlin:kotlin-stdlib-common:$kotlinVersion")
+        force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+        force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
+        force("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
     }
 }
 
 dependencies {
-    val kotlin_version: String by project
+    val kotlinVersion: String by project
     val okhttp_version: String by project
     val moshi_version: String by project
-    val junit_version: String by project
+    val junitVersion: String by project
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
     api(platform("com.squareup.okhttp3:okhttp-bom:$okhttp_version"))
     api("com.squareup.okhttp3:okhttp")
     api("com.squareup.moshi:moshi:$moshi_version")
     api("com.squareup.moshi:moshi-kotlin:$moshi_version")
 
     // tests
-    testImplementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    testImplementation("junit:junit:$junit_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    testImplementation("junit:junit:$junitVersion")
 }
