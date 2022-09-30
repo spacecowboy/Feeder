ff23128bd637fd4af6f434d8914622a18597ed97
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2022-09-30 20:52:51 +0200

    Changed user-agent to match Chrome's
    Fixes #590

--- app/src/main/java/com/nononsenseapps/feeder/model/Networking.kt
+++ app/src/main/java/com/nononsenseapps/feeder/model/Networking.kt
@@ -1,6 +1,5 @@
 package com.nononsenseapps.feeder.model
 
-import com.nononsenseapps.feeder.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -15,4 +14,6 @@ object UserAgentInterceptor : Interceptor {
     }
 }
 
-const val USER_AGENT_STRING = "Feeder / ${BuildConfig.VERSION_NAME}(${BuildConfig.VERSION_CODE})"
+// See discussion on https://gitlab.com/spacecowboy/Feeder/-/issues/590
+// const val USER_AGENT_STRING = "Feeder / ${BuildConfig.VERSION_NAME}(${BuildConfig.VERSION_CODE})"
+const val USER_AGENT_STRING = "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.136 Mobile Safari/537.36"
