d7f1af7c5bc6fa829156fb08053b5e701330c179
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2023-01-03 21:57:32 +0100

    Releasing 2.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
     namespace = "com.nononsenseapps.feeder"
     defaultConfig {
         applicationId = "com.nononsenseapps.feeder"
-        versionCode = 262
-        versionName = "2.4.2"
+        versionCode = 263
+        versionName = "2.4.3"
         compileSdk = 33
         minSdk = 23
         targetSdk = 33
