8adb4114c71ed801a517301c86c3eee98f370e54
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2023-01-24 21:46:35 +0100

    Updated UserAgent to avoid some issues with anti-spam
    Specifically the now removed plugin BadBehavior
    
    Fixes #710

--- app/src/main/java/com/nononsenseapps/feeder/model/Networking.kt
+++ app/src/main/java/com/nononsenseapps/feeder/model/Networking.kt
@@ -1,5 +1,6 @@
 package com.nononsenseapps.feeder.model
 
+import com.nononsenseapps.feeder.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -16,4 +17,6 @@ object UserAgentInterceptor : Interceptor {
 
 // See discussion on https://gitlab.com/spacecowboy/Feeder/-/issues/590
 // const val USER_AGENT_STRING = "Feeder / ${BuildConfig.VERSION_NAME}(${BuildConfig.VERSION_CODE})"
-const val USER_AGENT_STRING = "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.136 Mobile Safari/537.36"
+// See discussion on https://gitlab.com/spacecowboy/Feeder/-/issues/710
+// const val USER_AGENT_STRING = "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.136 Mobile Safari/537.36"
+const val USER_AGENT_STRING = "Mjukisbyxor / ${BuildConfig.VERSION_NAME}(${BuildConfig.VERSION_CODE})"
