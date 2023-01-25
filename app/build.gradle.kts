3acef0af5578e4dd32f5347a193f4e4cc8dc452d
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2023-01-25 09:32:19 +0100

    Releasing 2.4.6-1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
     namespace = "com.nononsenseapps.feeder"
     defaultConfig {
         applicationId = "com.nononsenseapps.feeder"
-        versionCode = 266
-        versionName = "2.4.6"
+        versionCode = 267
+        versionName = "2.4.6-1"
         compileSdk = 33
         minSdk = 23
         targetSdk = 33
