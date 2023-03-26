7a094e538e5bd3cb7b6c92dfb895c949b6566b11
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2023-03-26 10:28:47 +0200

    Releasing 2.4.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
     namespace = "com.nononsenseapps.feeder"
     defaultConfig {
         applicationId = "com.nononsenseapps.feeder"
-        versionCode = 272
-        versionName = "2.4.10"
+        versionCode = 273
+        versionName = "2.4.11"
         compileSdk = 33
         minSdk = 23
         targetSdk = 33
