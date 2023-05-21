41f1346e28a2c6094b5491942bac4d11c6a96727
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2023-05-21 17:34:27 +0200

    Releasing 2.4.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
     namespace = "com.nononsenseapps.feeder"
     defaultConfig {
         applicationId = "com.nononsenseapps.feeder"
-        versionCode = 276
-        versionName = "2.4.14"
+        versionCode = 277
+        versionName = "2.4.15"
         compileSdk = 33
         minSdk = 23
         targetSdk = 33
