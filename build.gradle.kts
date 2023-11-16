ae4b06544d0372c301c9b661ff32617f22015423
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2023-11-16 21:39:39 +0100

    Changed to ksp and upped kotlin and compose compile

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.kotlin.android).apply(false)
-    alias(libs.plugins.kotlin.kapt).apply(false)
+    alias(libs.plugins.kotlin.ksp).apply(false)
     alias(libs.plugins.kotlin.parcelize).apply(false)
     alias(libs.plugins.kotlin.jvm).apply(false)
 }
