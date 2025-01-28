fe46897f5959f6061f745dd67b14b73705367e2e
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2025-01-28 20:58:40 +0100

    fix: fixed so clicking outside selected text will dismiss it
    Signed-off-by: Jonas Kalderstam <jonas@cowboyprogrammer.org>

--- app/src/main/java/com/nononsenseapps/feeder/ui/compose/html/LinearArticleContent.kt
+++ app/src/main/java/com/nononsenseapps/feeder/ui/compose/html/LinearArticleContent.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ErrorOutline
@@ -41,13 +40,17 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.BaselineShift
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withLink
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
@@ -276,7 +279,8 @@ fun LinearVideoContent(
                             .clip(RectangleShape)
                             .clickable {
                                 linearVideo.firstSource.link.let(onLinkClick)
-                            }.fillMaxWidth(),
+                            }
+                            .fillMaxWidth(),
                 ) {
                     val maxImageWidth by rememberMaxImageWidth()
                     val pixelDensity = LocalDensity.current.density
@@ -420,7 +424,8 @@ fun LinearImageContent(
                             enabled = linearImage.link != null,
                         ) {
                             linearImage.link?.let(onLinkClick)
-                        }.fillMaxWidth(),
+                        }
+                        .fillMaxWidth(),
             ) {
                 val maxImageWidth by rememberMaxImageWidth()
                 val pixelDensity = LocalDensity.current.density
@@ -532,38 +537,16 @@ fun LinearTextContent(
     ProvideScaledText {
         WithBidiDeterminedLayoutDirection(linearText.text) {
             val interactionSource = remember { MutableInteractionSource() }
-
             val annotatedString = linearText.toAnnotatedString()
 
-            // ClickableText prevents taps from deselecting selected text
-            // So use regular Text if possible
-            if (linearText.annotations.any { it.data is LinearTextAnnotationLink }) {
-                ClickableText(
-                    text = annotatedString,
-                    softWrap = softWrap,
-                    style = LocalTextStyle.current,
-                    modifier =
-                        modifier
-                            .indication(interactionSource, LocalIndication.current)
-                            .focusableInNonTouchMode(interactionSource = interactionSource),
-                ) { offset ->
-                    annotatedString
-                        .getStringAnnotations("URL", offset, offset)
-                        .firstOrNull()
-                        ?.let {
-                            onLinkClick(it.item)
-                        }
-                }
-            } else {
-                Text(
-                    text = annotatedString,
-                    softWrap = softWrap,
-                    modifier =
-                        modifier
-                            .indication(interactionSource, LocalIndication.current)
-                            .focusableInNonTouchMode(interactionSource = interactionSource),
-                )
-            }
+            Text(
+                text = annotatedString,
+                softWrap = softWrap,
+                modifier =
+                    modifier
+                        .indication(interactionSource, LocalIndication.current)
+                        .focusableInNonTouchMode(interactionSource = interactionSource),
+            )
         }
     }
 }
@@ -605,19 +588,32 @@ fun LinearBlockQuoteContent(
                 }
 
             blockQuote.cite?.let { cite ->
-                ClickableText(
-                    text = AnnotatedString(cite),
-                    modifier = Modifier.align(Alignment.End),
-                    style =
-                        MaterialTheme.typography.bodySmall.merge(
-                            SpanStyle(
-                                color = MaterialTheme.colorScheme.tertiary,
-                                fontStyle = FontStyle.Italic,
+                val annotatedText =
+                    buildAnnotatedString {
+                        withLink(
+                            LinkAnnotation.Clickable(
+                                tag = cite,
+                                styles =
+                                    TextLinkStyles(
+                                        style =
+                                            MaterialTheme.typography.bodySmall.toSpanStyle().merge(
+                                                SpanStyle(
+                                                    color = MaterialTheme.colorScheme.tertiary,
+                                                    fontStyle = FontStyle.Italic,
+                                                ),
+                                            ),
+                                    ),
+                                linkInteractionListener = { onLinkClick(cite) },
                             ),
-                        ),
-                ) {
-                    onLinkClick(cite)
-                }
+                        ) {
+                            append(cite)
+                        }
+                    }
+
+                Text(
+                    text = annotatedText,
+                    modifier = Modifier.align(Alignment.End),
+                )
             }
         }
     }
@@ -692,10 +688,12 @@ fun CodeBlock(
                         } else {
                             this
                         }
-                    }.indication(
+                    }
+                    .indication(
                         interactionSource,
                         LocalIndication.current,
-                    ).focusableInNonTouchMode(interactionSource = interactionSource),
+                    )
+                    .focusableInNonTouchMode(interactionSource = interactionSource),
         ) {
             Box(
                 contentAlignment = Alignment.TopStart,
@@ -763,7 +761,8 @@ fun LinearTableContent(
                                     end = Offset(size.width, size.height),
                                 )
                             }
-                        }.padding(4.dp),
+                        }
+                        .padding(4.dp),
             ) {
                 val cellItem = linearTable.cellAt(row = row, col = column)
                 cellItem?.let {
@@ -866,17 +865,29 @@ fun LinearText.toAnnotatedString(): AnnotatedString {
             }
 
             is LinearTextAnnotationLink -> {
-                builder.addStringAnnotation(
-                    tag = "URL",
+                builder.addLink(
+                    url =
+                        LinkAnnotation.Url(
+                            url = data.href,
+                            styles =
+                                TextLinkStyles(
+                                    style = LinkTextStyle().toSpanStyle(),
+                                ),
+                        ),
                     start = annotation.start,
                     end = annotation.endExclusive,
-                    annotation = data.href,
                 )
-                builder.addStyle(
-                    start = annotation.start,
-                    end = annotation.endExclusive,
-                    style = LinkTextStyle().toSpanStyle(),
-                )
+//                builder.addStringAnnotation(
+//                    tag = "URL",
+//                    start = annotation.start,
+//                    end = annotation.endExclusive,
+//                    annotation = data.href,
+//                )
+//                builder.addStyle(
+//                    start = annotation.start,
+//                    end = annotation.endExclusive,
+//                    style = LinkTextStyle().toSpanStyle(),
+//                )
             }
 
             LinearTextAnnotationMonospace -> {
