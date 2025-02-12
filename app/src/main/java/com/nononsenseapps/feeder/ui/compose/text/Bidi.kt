9bc7a83ed51398017bcafba8b13fa65c83cf1cd1
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2025-02-12 13:22:05 +0100

    fix: fixed broken swipe to mark as read
    fixes #592
    
    Signed-off-by: Jonas Kalderstam <jonas@cowboyprogrammer.org>

--- app/src/main/java/com/nononsenseapps/feeder/ui/compose/text/Bidi.kt
+++ app/src/main/java/com/nononsenseapps/feeder/ui/compose/text/Bidi.kt
@@ -2,31 +2,38 @@ package com.nononsenseapps.feeder.ui.compose.text
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.movableContentOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.LayoutDirection
 import java.text.Bidi
 
 @Composable
-inline fun WithBidiDeterminedLayoutDirection(
+fun WithBidiDeterminedLayoutDirection(
     paragraph: String,
-    crossinline content: @Composable () -> Unit,
+    content: @Composable () -> Unit,
 ) {
     val bidi =
         remember(paragraph) {
             Bidi(paragraph, Bidi.DIRECTION_DEFAULT_LEFT_TO_RIGHT)
         }
 
-    val content = remember(paragraph) { movableContentOf { content() } }
-
     if (bidi.baseIsLeftToRight()) {
-        CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
-            content()
-        }
+        LeftToRight(content)
     } else {
-        CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
-            content()
-        }
+        RightToLeft(content)
+    }
+}
+
+@Composable
+fun LeftToRight(content: @Composable () -> Unit) {
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+        content()
+    }
+}
+
+@Composable
+fun RightToLeft(content: @Composable () -> Unit) {
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+        content()
     }
 }
